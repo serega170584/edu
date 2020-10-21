c917a3c28cdd88bcc8a36f0aebc5d8feb152bfce
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-10-21 15:42:00 +0300

    add educational organization module

---
+++ install/index.php
@@ -0,0 +1,54 @@
+<?php
+
+class educational_organization extends CModule
+{
+    var $MODULE_ID = "educational_organization";
+    var $MODULE_VERSION;
+    var $MODULE_VERSION_DATE;
+    var $MODULE_NAME;
+    var $MODULE_DESCRIPTION;
+    var $MODULE_CSS;
+
+    function educational_organization()
+    {
+        $arModuleVersion = array();
+
+        $path = str_replace("\\", "/", __FILE__);
+        $path = substr($path, 0, strlen($path) - strlen("/index.php"));
+        include($path . "/version.php");
+
+        if (is_array($arModuleVersion) && array_key_exists("VERSION", $arModuleVersion)) {
+            $this->MODULE_VERSION = $arModuleVersion["VERSION"];
+            $this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
+        }
+
+        $this->MODULE_NAME = "educational_organization – модуль образовательной организации";
+        $this->MODULE_DESCRIPTION = "После установки вы сможете пользоваться модулем образовательной организации";
+    }
+
+    function InstallFiles()
+    {
+        return true;
+    }
+
+    function UnInstallFiles()
+    {
+        return true;
+    }
+
+    function DoInstall()
+    {
+        global $DOCUMENT_ROOT, $APPLICATION;
+        $this->InstallFiles();
+        RegisterModule("educational_organization");
+        $APPLICATION->IncludeAdminFile("Установка модуля educational_organization", $DOCUMENT_ROOT . "/local/modules/educational_organization/install/step.php");
+    }
+
+    function DoUninstall()
+    {
+        global $DOCUMENT_ROOT, $APPLICATION;
+        $this->UnInstallFiles();
+        UnRegisterModule("educational_organization");
+        $APPLICATION->IncludeAdminFile("Деинсталляция модуля educational_organization", $DOCUMENT_ROOT . "/local/modules/educational_organization/install/unstep.php");
+    }
+}
---
+++ install/step.php
@@ -0,0 +1,4 @@
+<?php if(!check_bitrix_sessid()) return;?>
+<?php
+echo CAdminMessage::ShowNote("Модуль educational_organization установлен");
+?>
