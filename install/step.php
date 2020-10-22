40c3173f90773d9244a2591733555f63deceaae2
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-10-22 19:42:56 +0300

    check install

--- install/step.php
+++
@@ -1,7 +0,0 @@
-<?php if (!check_bitrix_sessid()) return;
-/**
- * @var \CMain $APPLICATION
- * @var string $DOCUMENT_ROOT
- */
-$APPLICATION->IncludeAdminFile("Добавление типа инфоблока образовательной организации", $DOCUMENT_ROOT . "/local/modules/educational_organization/install/add_infoblock_type_step.php");
-?>
--- install/step1.php
+++ install/step1.php
@@ -1,1 +1,7 @@
-<?php
+<?php if (!check_bitrix_sessid()) return;
+/**
+ * @var \CMain $APPLICATION
+ * @var string $DOCUMENT_ROOT
+ */
+$APPLICATION->IncludeAdminFile("Добавление типа инфоблока образовательной организации", $DOCUMENT_ROOT . "/local/modules/educational_organization/install/add_infoblock_type_step.php");
+?>
