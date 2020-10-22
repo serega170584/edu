4c9ef77eb033f389c89dc284b82e599e5861096e
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-10-22 18:19:55 +0300

    check install

--- install/index.php
+++ install/index.php
@@ -61,7 +61,6 @@ class educational_organization extends CModule
          */
         global $DOCUMENT_ROOT, $APPLICATION, $DB;
         try {
-            throw new \Exception('Ошибка');
             $DB->StartTransaction();
 //            $this->UnInstallFiles();
             UnRegisterModule("educational_organization");
