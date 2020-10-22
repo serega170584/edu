715c636ae0c5d647ed16367d762b69ab4ae3539c
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-10-22 14:23:11 +0300

    check install

--- install/index.php
+++ install/index.php
@@ -44,6 +44,7 @@ class educational_organization extends CModule
          * \CDatabase $DB
          */
         global $DOCUMENT_ROOT, $APPLICATION, $DB;
+        die('asd');
         try {
             $DB->StartTransaction();
             $APPLICATION->IncludeAdminFile("Установка модуля educational_organization", $DOCUMENT_ROOT . "/local/modules/educational_organization/install/step.php");
