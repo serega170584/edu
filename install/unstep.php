f8b94bd7f0a5254d69714f02efafb7f6439b3e86
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-10-22 16:32:48 +0300

    check install

--- install/unstep.php
+++ install/unstep.php
@@ -4,6 +4,6 @@
  * @var \CMain $APPLICATION
  * @var string $DOCUMENT_ROOT
  */
-//$APPLICATION->IncludeAdminFile("Удаление типа инфоблока образовательной организации", $DOCUMENT_ROOT . "/local/modules/educational_organization/install/add_infoblock_type_unstep.php");
+$APPLICATION->IncludeAdminFile("Удаление типа инфоблока образовательной организации", $DOCUMENT_ROOT . "/local/modules/educational_organization/install/add_infoblock_type_unstep.php");
 echo \CAdminMessage::ShowNote("Модуль успешно удален из системы");
 ?>
