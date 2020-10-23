bbd344a4341e21ecbf5326bae6158fd6ffa2188a
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-10-23 15:52:23 +0300

    add user fields

--- install/step.php
+++ install/step.php
@@ -6,6 +6,7 @@
  * @var string $moduleId
  */
 global $moduleId;
+echo \CAdminMessage::ShowNote("Добавление типа инфоблока образовательной организации");
 $arFields = [
     'ID' => $moduleId,
     'SECTIONS' => 'Y',
@@ -30,6 +31,49 @@ if (!$res) {
     $DB->Rollback();
     throw new \Bitrix\Main\DB\Exception($obBlocktype->LAST_ERROR);
 }
-$DB->Commit();
 echo \CAdminMessage::ShowNote("Тип инфоблока образовательной организации добавлен");
+
+echo \CAdminMessage::ShowNote("Добавление свойств: время начала работы, время окончания работы, сайт пользователя");
+$oUserTypeEntity = new CUserTypeEntity();
+$aUserFields = [
+    'ENTITY_ID' => 'USER',
+    'FIELD_NAME' => 'UF_BEGIN_TIME',
+    'USER_TYPE_ID' => 'datetime',
+    'XML_ID' => 'BEGIN_DATE',
+    'SORT' => 500,
+    'MULTIPLE' => 'N',
+    'MANDATORY' => 'N',
+    'SHOW_FILTER' => 'N',
+    'SHOW_IN_LIST' => '',
+    'EDIT_IN_LIST' => '',
+    'IS_SEARCHABLE' => 'N',
+    'EDIT_FORM_LABEL' => [
+        'ru' => 'Время начала',
+        'en' => 'Begin time'
+    ],
+    'LIST_COLUMN_LABEL' => [
+        'ru' => 'Время начала',
+        'en' => 'Begin time',
+    ],
+    'LIST_FILTER_LABEL' => [
+        'ru' => 'Время начала',
+        'en' => 'Begin time',
+    ],
+    'ERROR_MESSAGE' => [
+        'ru' => 'Ошибка при заполнении',
+        'en' => 'An error in completing',
+    ],
+    'HELP_MESSAGE' => [
+        'ru' => '',
+        'en' => '',
+    ]
+];
+$res = $oUserTypeEntity->Add($aUserFields);
+if (!$res) {
+    $DB->Rollback();
+    throw new \Bitrix\Main\DB\Exception('Ошибка добавления пользовательского свойства');
+}
+echo \CAdminMessage::ShowNote("Свойства пользователя добавлены");
+
+$DB->Commit();
 ?>
--- install/unstep.php
+++ install/unstep.php
@@ -7,10 +7,21 @@
  * @var string $moduleId
  */
 global $moduleId;
+$oUserTypeEntity = new CUserTypeEntity();
+echo \CAdminMessage::ShowNote("Удаление свойств: время начала работы, время окончания работы, сайт пользователя");
+$oUserTypeEntity->Delete(CUserTypeEntity::GetList([
+    'ID' => 'ASC'
+], [
+    'ENTITY_ID' => 'USER',
+    'FIELD_NAME' => 'UF_BEGIN_TIME'
+])->Fetch()['ID']);
+echo \CAdminMessage::ShowNote("Свойства пользователя удалены");
+
+echo \CAdminMessage::ShowNote("Удаление типа инфоблока образовательной организации");
 if (!\CIBlockType::Delete($moduleId)) {
     $DB->Rollback();
     throw new \Bitrix\Main\DB\Exception('Delete error!');
 }
+echo \CAdminMessage::ShowNote("Тип инфоблока образовательной организации удален");
 $DB->Commit();
-echo \CAdminMessage::ShowNote("Тип инфоблока образовательной организации успешно удален из системы");
 ?>
