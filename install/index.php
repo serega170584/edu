3d75b36efb4d5bdfa3a64d07f4289bb8d1b8c855
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-11 09:29:14 +0300

    check add iblock property aspect

--- install/index.php
+++ install/index.php
@@ -547,6 +547,7 @@ class Edu extends CModule
         }
         $id = $property->Add($arFields);
         if (!($id > 0)) {
+            var_dump($DB->GetErrorSQL());
             var_dump($DB->GetErrorMessage());
             $DB->Rollback();
             throw new \Bitrix\Main\DB\Exception(GetMessage('db_add_infoblock_property_error'));
